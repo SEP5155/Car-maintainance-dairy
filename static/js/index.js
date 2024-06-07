3627f3bcbcb9c31604ca7727c2004472342c63e7
SEP5155 <serg.papusha@gmail.com>
2024-06-07 03:09:15 +0300

    added function that show custom alert on every page

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,17 @@
-console.log('testing js connection');
+document.addEventListener('DOMContentLoaded', function() {
+
+    // console.log('testing js connection');
+
+    function customAlert() {
+        const customAlert = document.querySelector('.custom_alert');
+        const pageName = document.querySelector('h1.page_title').innerHTML;
+        customAlert.innerHTML = ('You are at ' + pageName + ' page right now');
+        customAlert.classList.add('show');
+
+        setTimeout(function() {
+            customAlert.classList.remove('show');
+        }, 5000);
+    }
+    customAlert();
+})
+
