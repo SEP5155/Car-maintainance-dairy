85d6641959a1d6c9004b22dddbe9c29f5f71418c
SEP5155 <serg.papusha@gmail.com>
2024-06-07 18:02:07 +0300

    added slider for users and script that handles it

--- static/js/index.js
+++ static/js/index.js
@@ -40,6 +40,29 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     }
+    //SLIDER FOR HOME PAGE
+    let currentIndex = 0;
+    let images = document.querySelectorAll('.random_car-img');
+    let totalImages = images.length;
+    console.log('Images found: ' + totalImages);
+    let nextBtn = document.getElementById('next-button');
+    let prevBtn = document.getElementById('prev-button');
+
+    function showNext() {
+        images[currentIndex].classList.remove('active');
+        currentIndex = (currentIndex + 1) % totalImages;
+        images[currentIndex].classList.add('active');
+        console.log('showing slide number' + (currentIndex + 1));
+    }
+    function showPrev() {
+        images[currentIndex].classList.remove('active');
+        currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+        images[currentIndex].classList.add('active');
+        console.log('showing slide number' + (currentIndex + 1));
+    }
+    nextBtn.addEventListener('click', showNext);
+    prevBtn.addEventListener('click', showPrev);
+
     
     
     customAlert();
