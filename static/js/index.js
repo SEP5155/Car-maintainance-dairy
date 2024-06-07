e7d9244b391680343f61db77259be827d28eabf0
SEP5155 <serg.papusha@gmail.com>
2024-06-07 04:09:09 +0300

    adjusted style for nav menu and alert block

--- static/js/index.js
+++ static/js/index.js
@@ -7,27 +7,34 @@ document.addEventListener('DOMContentLoaded', function() {
         const pageName = document.querySelector('h1.page_title').innerHTML;
         customAlert.innerHTML = ('You are at ' + pageName + ' page right now');
         customAlert.classList.add('show');
+        console.log('showing alert');
 
         setTimeout(function() {
             customAlert.classList.remove('show');
+            console.log('hiding alert');
         }, 5000);
     }
 
     const extraInput = document.querySelectorAll('.register_extra_info');
     const extraMenuCheckbox = document.querySelector('#tell_more');
 
-    extraMenuCheckbox.addEventListener('change', function() {
-        if (extraMenuCheckbox.checked) {
-            extraInput.forEach(function(element) {
-                element.style.display = 'inline-block';
-            });
-            
-        } else {
-            extraInput.forEach(function(element) {
-                element.style.display = 'none';
-            });
-        }
-    });
+    if(extraMenuCheckbox) {
+        extraMenuCheckbox.addEventListener('change', function() {
+            if (extraMenuCheckbox.checked) {
+                extraInput.forEach(function(element) {
+                    element.style.display = 'inline-block';
+                    console.log('showing extra menu');
+                });
+                
+            } else {
+                extraInput.forEach(function(element) {
+                    element.style.display = 'none';
+                    console.log('hiding extra menu');
+                });
+            }
+        });
+    }
+    
     
     customAlert();
   
