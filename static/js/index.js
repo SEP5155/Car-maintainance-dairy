1f3c1fd1abd649f67eaa01b2d0203f75d3d5ca72
SEP5155 <serg.papusha@gmail.com>
2024-06-25 02:23:12 +0300

    added check for soting script to avoid errors in console

--- static/js/index.js
+++ static/js/index.js
@@ -117,32 +117,33 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 
-    document.getElementById("sort_entries").addEventListener("change", function() {
-        console.log(this.value);
-        let criteria = this.value;
-        let entries = Array.from(document.querySelectorAll('.posts .post'));
-
-        entries.sort(function(a, b) {
-            let valueA = a.querySelector("." + criteria).textContent;
-            let valueB = b.querySelector("." + criteria).textContent;
-
-            if (criteria === "date") {
-                return new Date(valueA) - new Date(valueB);
-
-            } else {
-                return parseFloat(valueA) - parseFloat(valueB);
-            }
-        });
-
-        let container = document.querySelector(".posts");
-        container.innerHTML = "";
-        entries.forEach(function(entry) {
-            container.appendChild(entry);
-        });
-
-    })
+    const sortMenu = document.getElementById("sort_entries");
+    if(sortMenu) {
+        sortMenu.addEventListener("change", function() {
+            console.log(this.value);
+            let criteria = this.value;
+            let entries = Array.from(document.querySelectorAll('.posts .post'));
     
+            entries.sort(function(a, b) {
+                let valueA = a.querySelector("." + criteria).textContent;
+                let valueB = b.querySelector("." + criteria).textContent;
     
-
+                if (criteria === "date") {
+                    return new Date(valueA) - new Date(valueB);
+    
+                } else {
+                    return parseFloat(valueA) - parseFloat(valueB);
+                }
+            });
+    
+            let container = document.querySelector(".posts");
+            container.innerHTML = "";
+            entries.forEach(function(entry) {
+                container.appendChild(entry);
+            });
+    
+        })
+    }
+    
 });
 
