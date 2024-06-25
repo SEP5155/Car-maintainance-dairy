99d85592c47776f4fd0b29b824e27c4e5112c8cf
SEP5155 <serg.papusha@gmail.com>
2024-06-26 00:15:05 +0300

    added script that fetching pictures depending on bady marking, added values to db, but it's conflicting with backend and since page reloaded it does not work, added data to template to adjust src to img depending on current car, I would need either remove fetch or make this form not to reload page when updating data

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    document.getElementById('user_cars').addEventListener('change', function() {
+        let selectedIndex = this.selectedIndex;
+
+        // Получаем выбранный элемент <option> по индексу
+        let selectedOption = this.options[selectedIndex];
+
+        // Получаем id выбранного элемента <option>
+        let selectedBrand = selectedOption.id;
+        let carImage = document.getElementById('current_car_img');
+        let loadingMsg = document.getElementById('loadingMsg');
+
+        loadingMsg.style.display = 'block';
+        carImage.style.display = 'none';
+
+        fetch('/static/img/default_brand/' + selectedBrand + '.jpg')
+            .then(function(response) {
+                if (response.ok) {
+                    return response.blob();
+                } else {
+                    throw new Error('Image not found ' + response.status);
+                }
+            })
+            .then(function(imageBlob) {
+                let imageUrl = URL.createObjectURL(imageBlob);
+                carImage.src = imageUrl;
+
+                loadingMsg.style.display = 'none';
+                carImage.style.display = 'block';
+            })
+            .catch(function(error) {
+                console.error('Error loading image:', error);
+                loadingMsg.textContent = 'Error loading image'
+            })
+    })
+
+
     // console.log('testing js connection');
 
     function customAlert() {
